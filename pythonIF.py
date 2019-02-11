a833324972793b7ad771af545da2a56daceabdbd
Raees123 <raiskochargi@gmail.com>
2019-02-11 13:26:28 +0530

    if in pyhton

---
+++ pythonIF.py
@@ -0,0 +1,8 @@
+person = input("Nationality?")
+if person == "french" or person == "French":
+    print("Welocme,frnech")
+elif person == "italian" or person == "Italian":
+    print("Welcome,italian")
+else:
+    print("you are neither italian nor french,")
+    print("so we have to speak English with each other.")
